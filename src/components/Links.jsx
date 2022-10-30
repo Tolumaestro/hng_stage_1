0a2779bf8b684ceee556f7d3b48cb3851d6f0b77
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-10-30 14:47:56 +0100

    created the profile section

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Links = () => {
-  return <div>Links</div>;
+  return (
+    <div>
+      <a href="https://training.zuri.team/">Zuri Team</a>
+    </div>
+  );
 };
 
 export default Links;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import profileImg from "../assets/profile__img.png";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-6">
+      <img id="profile__img" className="rounded-full" src={profileImg} alt="" />
+      <h1 id="twitter" className="text-xl font-bold">
+        tolu_soetan
+      </h1>
+      <h1 id="slack" className="hidden">
+        Tolumeastro
+      </h1>
+    </div>
+  );
 };
 
 export default Profile;
